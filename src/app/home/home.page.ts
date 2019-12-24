7f1f1bb4a0add0c2ee0dc4fc37908c10e20b3483
Gjoko <gjoko@inteli.mk>
2019-12-24 17:01:26 +0100

    days layout and css snowflakes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ToastController, AlertController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  doors = [...Array(31).keys()];
+  currentDate = new Date();
+  constructor(
+    private toastCtrl: ToastController,
+    private alertCtrl: AlertController
+  ) {}
 
-  constructor() {}
+  async openDoor(index) {
+    console.log("door", index + 1);
+    if (this.closed(index)) {
+      const toast = await this.toastCtrl.create({
+        header: "Just a few more days...",
+        duration: 2000
+      });
+      toast.present();
+    } else {
+      const alert = await this.alertCtrl.create({
+        header: `Day ${index + 1}`,
+        message: "Ho ho ho!",
+        buttons: ["Ok"]
+      });
+      alert.present();
+    }
+  }
 
+  closed(day) {
+    return this.currentDate.getDate() < day + 1;
+  
+  }
 }
